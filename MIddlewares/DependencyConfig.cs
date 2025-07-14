21ece4fdcd0e948bf50f58a1ccd7d37746cf28cf
sunflower <hoanganh220104@gmail.com>
2025-07-14 10:03:58 +0700

    Update Program.cs, HomeController and Views

---
+++ MIddlewares/DependencyConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace GGStore.MIddlewares
+{
+    public class DependencyConfig
+    {
+        public static void RegisterDependency(IServiceCollection services)
+        {
+            BLL.Extensions.ServiceCollectionExtensions.AddBussinessLogicLayer(services, null);
+            DAL.Extensions.ServiceCollectionExtensions.AddDataAccessLayer(services, null);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,21 @@
+using BLL.Mapping;
+using GGStore.MIddlewares;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
+//Dependency Injection
+DependencyConfig.RegisterDependency(builder.Services);
+
+//Auto Mapper
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Homepage}"
+    );
 
 app.Run();
+
